9ffa8bc4162ae8fc9c844a78ef419b1f13ecce1d
oaao <oliver+github@abolish.golf>
2026-03-19 17:51:45 -0600

    fix [08 - routing]: ex01 - inline page component exports

--- 08 - routing/examples/01 - static routing/instructor/frontend/src/pages/AdminPage.jsx
+++ 08 - routing/examples/01 - static routing/instructor/frontend/src/pages/AdminPage.jsx
@@ -1,5 +1,14 @@
-export default AdminPage = () => {
+// I could do export const AdminPage, but you'd be forced to use named imports
+//   import { AdminPage } from '@/pages/AdminPage';
+
+const AdminPage = () => {
   return (
     <h1>Admin Page</h1>
   );
 };
+
+export default AdminPage
+
+// you *can* export default someFunction() {}   (i.e. not arrow functions)
+// but I'm always trying to show you multiple ways of doing things, so today
+// our inline page components are arrow functions.
